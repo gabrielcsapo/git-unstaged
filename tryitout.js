3da5109f70b06ac415c5d40228ec560ea4cd8755
Gabriel Csapo <gabecsapo@gmail.com>
2017-10-08 14:15:19 -0700

    adds demo page

---
+++ tryitout.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: "git-unstaged",
+  description: "🎭 Get all unstaged git repos in a folder",
+  sourceCodeLink: 'https://github.com/gabrielcsapo/git-unstaged',
+  downloadLink: 'https://github.com/gabrielcsapo/git-unstaged/releases',
+  icon: '',
+  demoImage: './example.gif'
+}
